930112e4f4359430ce689ef5da4b52213f6416be
Markus Frei <markus.frei@linuxfabrik.ch>
2020-04-03 09:00:33 +0200

    new function get_worst()

--- globals.py
+++ globals.py
@@ -35,3 +35,19 @@ def get_greater_state(first_state, second_state):
         return second_state
 
     return first_state
+
+
+def get_worst(state1, state2):
+    """Get the more worst of two states, in this particular order:
+        # CRIT    (2)
+        # WARN    (1)
+        # UNKNOWN (3)
+        # OK      (0)
+    """
+    if state1 == STATE_CRIT or state2 == STATE_CRIT:
+        return STATE_CRIT
+    if state1 == STATE_WARN or state2 == STATE_WARN:
+        return STATE_WARN
+    if state1 == STATE_UNKNOWN or state2 == STATE_UNKNOWN:
+        return STATE_UNKNOWN
+    return STATE_OK
