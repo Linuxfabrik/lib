65db92ce5e346ec1dee42768fe4d1dc3e8f469ca
Markus Frei <markus.frei@linuxfabrik.ch>
2020-04-30 15:08:31 +0200

    deactivated STATE_DEPENDENT

--- globals.py
+++ globals.py
@@ -8,12 +8,12 @@
 
 # https://git.linuxfabrik.ch/linuxfabrik-icinga-plugins/checks-linux/-/blob/master/CONTRIBUTING.md
 
-__author__  = 'Linuxfabrik GmbH, Zurich/Switzerland'
-__version__ = '2020040701'
+__author__ = 'Linuxfabrik GmbH, Zurich/Switzerland'
+__version__ = '2020043001'
 
 
 STATE_OK        = 0
 STATE_WARN      = 1
 STATE_CRIT      = 2
 STATE_UNKNOWN   = 3
-STATE_DEPENDENT = 4
+#STATE_DEPENDENT = 4
