240ca99ba186959a9034f6cf440edc6bcc74e22b
navid.sassan <navid.sassan@linuxfabrik.ch>
2021-02-15 11:08:21 +0100

    smb: added glob function

--- smb2.py
+++ smb2.py
@@ -12,7 +12,7 @@
 """
 
 __author__ = 'Linuxfabrik GmbH, Zurich/Switzerland'
-__version__ = '2021021101'
+__version__ = '2021021501'
 
 
 import warnings
@@ -49,6 +49,43 @@ def open_file(path, username, password, timeout, encrypt=True):
             return (False, 'The file that was specified as a target is a directory, should be a file.')
         if isinstance(e.__context__, smbprotocol.exceptions.ObjectNameNotFound):
             return (False, 'No such file or directory on the smb server.')
+        return (False, 'I/O error "{}" while opening or reading {}'.format(e.strerror, path))
+    except Exception as e:
+        return (False, 'Unknown error opening or reading {}:\n{}'.format(path, e))
+
+
+def glob(path, username, password, timeout, pattern='*', encrypt=True):
+    if missing_smb_lib:
+        return (False, 'Python module "{}" is not installed.'.format(missing_smb_lib))
+    try:
+        file_entry = smbclient._os.SMBDirEntry.from_path(
+                path,
+                username=username,
+                password=password,
+                connection_timeout=timeout,
+                encrypt=encrypt,
+            )
+
+        if file_entry.is_file():
+            return (True, [file_entry])
+
+        # converting generator to list here to trigger any exception when accessing files now. this could probably be improved
+        return (True, list(smbclient.scandir(
+                path,
+                mode='rb',
+                username=username,
+                password=password,
+                connection_timeout=timeout,
+                search_pattern=pattern,
+                encrypt=encrypt,
+            )))
+    except (smbprotocol.exceptions.SMBAuthenticationError, smbprotocol.exceptions.LogonFailure):
+        return (False, 'Login failed')
+    except smbprotocol.exceptions.SMBOSError as e:
+        if isinstance(e.__context__, smbprotocol.exceptions.ObjectNameNotFound):
+            return (False, 'No such file or directory on the smb server.')
+        if e.strerror == 'No such file or directory':
+            return (True, [])
         return (False, 'I/O error "{}" while opening or reading {}'.format(e.strerror, path))
     except Exception as e:
         return (False, 'Unknown error opening or reading {}:\n{}'.format(path, e))
