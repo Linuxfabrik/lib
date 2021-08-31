686dbb50adbcc7707a469f62ebcb5cca21f4438f
Markus Frei <markus.frei@linuxfabrik.ch>
2021-08-31 16:03:15 +0200

    get_table(): Just use ASCII chars, as this is the lowest common denominator across all terminals (fixed #33)

--- base3.py
+++ base3.py
@@ -12,7 +12,7 @@
 """
 
 __author__ = 'Linuxfabrik GmbH, Zurich/Switzerland'
-__version__ = '2021082601'
+__version__ = '2021083101'
 
 import collections
 import datetime
@@ -430,7 +430,7 @@ def get_table(data, cols, header=None, strip=True, sort_by_key=None, sort_order_
         # Get the length of each column and create a '---' divider based on that length
         header_divider = []
         for col, width in column_widths.items():
-            header_divider.append('─' * width)
+            header_divider.append('-' * width)
 
         # Insert the header divider below the header row
         header_divider = dict(zip(cols, header_divider))
@@ -443,10 +443,10 @@ def get_table(data, cols, header=None, strip=True, sort_by_key=None, sort_order_
         tmp = ''
         for col, width in column_widths.items():
             if cnt != 1:
-                tmp += '{:<{}} │ '.format(row[col], width)
+                tmp += '{:<{}} ! '.format(row[col], width)
             else:
                 # header row
-                tmp += '{:<{}}─┼─'.format(row[col], width)
+                tmp += '{:<{}}-+-'.format(row[col], width)
         cnt += 1
         table += tmp[:-2] + '\n'
 
