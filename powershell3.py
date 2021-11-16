c66de91cfa7f6dd121f497303a51555e7e6e259f
Markus Frei <markus.frei@linuxfabrik.ch>
2021-11-16 06:18:28 +0100

    Support PowerShell (fixed #40)

---
+++ powershell3.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8; py-indent-offset: 4 -*-
+#
+# Author:  Linuxfabrik GmbH, Zurich, Switzerland
+# Contact: info (at) linuxfabrik (dot) ch
+#          https://www.linuxfabrik.ch/
+# License: The Unlicense, see LICENSE file.
+
+# https://git.linuxfabrik.ch/linuxfabrik-icinga-plugins/checks-linux/-/blob/master/CONTRIBUTING.md
+
+"""This library collects some Microsoft PowerShell related functions.
+"""
+
+__author__ = 'Linuxfabrik GmbH, Zurich/Switzerland'
+__version__ = '2021111501'
+
+import subprocess
+
+
+def run_cmdlet(cmd):
+    """
+    You will need PowerShell installed on your system and Python 3.6+.
+    This would work cross-platform. No need for external libraries.
+
+    Returns
+    * result.args (list)
+    * result.returncode: 0 = ok
+    * result.stdout: Byte-String
+    * result.stderr: Byte-String
+    """
+    result = subprocess.run(['powershell', '-Command', cmd], capture_output=True)
+    return {
+        'args': result.args,
+        'retc': result.returncode,
+        'stdout': result.stdout.decode(), # convert from byte to unicode
+        'stderr': result.stderr.decode(), # convert from byte to unicode
+    }
