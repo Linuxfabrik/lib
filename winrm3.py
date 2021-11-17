eabf389d5b55b4b3386d2b3362d84925852a9039
Markus Frei <markus.frei@linuxfabrik.ch>
2021-11-17 15:17:50 +0100

    winrm: Add function to run shell commands (fixed #41)

--- winrm3.py
+++ winrm3.py
@@ -12,7 +12,7 @@
 """
 
 __author__ = 'Linuxfabrik GmbH, Zurich/Switzerland'
-__version__ = '2021111701'
+__version__ = '2021111702'
 
 try:
     import winrm
@@ -22,7 +22,7 @@ else:
     winrm_found = True
 
 
-def run_ps(cmd, args):
+def run_ps(args, cmd):
     """Returns a winrm object.
     * result.status_code
     * result.std_out
@@ -34,6 +34,27 @@ def run_ps(cmd, args):
         else:
             session = winrm.Session(args.WINRM_HOSTNAME, auth=(args.WINRM_USERNAME, args.WINRM_PASSWORD), transport=args.WINRM_TRANSPORT)
         result = session.run_ps(cmd) # run Powershell block
+        return {
+            'retc': result.status_code,
+            'stdout': result.std_out.decode(), # convert from byte to unicode
+            'stderr': result.std_err.decode(), # convert from byte to unicode
+        }
+    except:
+        return None
+
+
+def run_cmd(args, cmd, params=[]):
+    """Returns a winrm object.
+    * result.status_code
+    * result.std_out
+    * result.std_err
+    """
+    try:
+        if args.WINRM_DOMAIN:
+            session = winrm.Session(args.WINRM_HOSTNAME, auth=('{}@{}'.format(args.WINRM_USERNAME, args.WINRM_DOMAIN), args.WINRM_PASSWORD), transport=args.WINRM_TRANSPORT)
+        else:
+            session = winrm.Session(args.WINRM_HOSTNAME, auth=(args.WINRM_USERNAME, args.WINRM_PASSWORD), transport=args.WINRM_TRANSPORT)
+        result = session.run_cmd(cmd, params) # run command in cmd.exe
         return {
             'retc': result.status_code,
             'stdout': result.std_out.decode(), # convert from byte to unicode
