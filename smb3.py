8d8af358524c5a914ec3e4de637044f29fec9ae9
Markus Frei <markus.frei@linuxfabrik.ch>
2022-04-05 11:40:13 +0200

    Revert Python 3.6+ f-strings to use .format() to be more conservative

--- smb3.py
+++ smb3.py
@@ -12,7 +12,7 @@
 """
 
 __author__ = 'Linuxfabrik GmbH, Zurich/Switzerland'
-__version__ = '2022021601'
+__version__ = '2022040501'
 
 missing_smb_lib = False
 try:
@@ -24,16 +24,16 @@ except ImportError as e:
 
 def open_file(path, username, password, timeout, encrypt=True):
     if missing_smb_lib:
-        return (False, f'Python module "{missing_smb_lib}" is not installed.')
+        return (False, 'Python module "{}" is not installed.'.format(missing_smb_lib))
     try:
         return (True, smbclient.open_file(
-                path,
-                mode='rb',
-                username=username,
-                password=password,
-                connection_timeout=timeout,
-                encrypt=encrypt,
-            ))
+            path,
+            mode='rb',
+            username=username,
+            password=password,
+            connection_timeout=timeout,
+            encrypt=encrypt,
+        ))
     except (smbprotocol.exceptions.SMBAuthenticationError, smbprotocol.exceptions.LogonFailure):
         return (False, 'Login failed')
     except smbprotocol.exceptions.SMBOSError as e:
@@ -41,36 +41,36 @@ def open_file(path, username, password, timeout, encrypt=True):
             return (False, 'The file that was specified as a target is a directory, should be a file.')
         if isinstance(e.__context__, smbprotocol.exceptions.ObjectNameNotFound):
             return (False, 'No such file or directory on the smb server.')
-        return (False, f'I/O error "{e.strerror}" while opening or reading {path}')
+        return (False, 'I/O error "{}" while opening or reading {}'.format(e.strerror, path))
     except Exception as e:
-        return (False, f'Unknown error opening or reading {path}:\n{e}')
+        return (False, 'Unknown error opening or reading {}:\n{}'.format(path, e))
 
 
 def glob(path, username, password, timeout, pattern='*', encrypt=True):
     if missing_smb_lib:
-        return (False, f'Python module "{missing_smb_lib}" is not installed.')
+        return (False, 'Python module "{}" is not installed.'.format(missing_smb_lib))
     try:
         file_entry = smbclient._os.SMBDirEntry.from_path(
-                path,
-                username=username,
-                password=password,
-                connection_timeout=timeout,
-                encrypt=encrypt,
-            )
+            path,
+            username=username,
+            password=password,
+            connection_timeout=timeout,
+            encrypt=encrypt,
+        )
 
         if file_entry.is_file():
             return (True, [file_entry])
 
         # converting generator to list here to trigger any exception when accessing files now. this could probably be improved
         return (True, list(smbclient.scandir(
-                path,
-                mode='rb',
-                username=username,
-                password=password,
-                connection_timeout=timeout,
-                search_pattern=pattern,
-                encrypt=encrypt,
-            )))
+            path,
+            mode='rb',
+            username=username,
+            password=password,
+            connection_timeout=timeout,
+            search_pattern=pattern,
+            encrypt=encrypt,
+        )))
     except (smbprotocol.exceptions.SMBAuthenticationError, smbprotocol.exceptions.LogonFailure):
         return (False, 'Login failed')
     except smbprotocol.exceptions.SMBOSError as e:
@@ -78,6 +78,6 @@ def glob(path, username, password, timeout, pattern='*', encrypt=True):
             return (False, 'No such file or directory on the smb server.')
         if e.strerror == 'No such file or directory':
             return (True, [])
-        return (False, f'I/O error "{e.strerror}" while opening or reading {path}')
+        return (False, 'I/O error "{}" while opening or reading {}'.format(e.strerror, path))
     except Exception as e:
-        return (False, f'Unknown error opening or reading {path}:\n{e}')
+        return (False, 'Unknown error opening or reading {}:\n{}'.format(path, e))
