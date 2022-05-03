32d3479ee9ebd101883d9f7bbfbcc53824a4e238
Markus Frei <markus.frei@linuxfabrik.ch>
2022-05-03 12:30:19 +0200

    db_mysql3.py: Enhanced for new mysql-checks

--- db_mysql3.py
+++ db_mysql3.py
@@ -15,7 +15,7 @@ https://dev.mysql.com/doc/connector-python/en/
 """
 
 __author__ = 'Linuxfabrik GmbH, Zurich/Switzerland'
-__version__ = '2022021701'
+__version__ = '2022050201'
 
 from .globals3 import STATE_UNKNOWN
 
@@ -35,6 +35,22 @@ if HAVE_MYSQL_CONNECTOR and base3.version(mysql.connector.__version__) < base3.v
         HAVE_MYSQL_CURSORS = False
 else:
     HAVE_MYSQL_CURSORS = None
+
+if HAVE_MYSQL_CONNECTOR is False:
+    base3.oao('Python module "mysql.connector" is not installed.', STATE_UNKNOWN)
+if HAVE_MYSQL_CONNECTOR is False:
+    base3.oao('Python module "MySQLdb.cursors" is not installed.', STATE_UNKNOWN)
+
+
+def check_select_privileges(conn):
+    success, result = select(
+        conn,
+        'select version();',
+        fetchone=True,
+    )
+    if not success or len(result) < 1:
+        return (False, 'You probably did not get enough privileges for running SELECT statements.')
+    return (True, True)
 
 
 def close(conn):
@@ -67,20 +83,24 @@ def connect(mysql_connection):
     ...     'password':           args3.PASSWORD,
     ...     'host':               args3.HOSTNAME,
     ...     'database':           args3.DATABASE,
-    ...     'raise_on_warnings':  True
+    ...     'raise_on_warnings':  True,
     ... }
     >>> conn = connect(mysql_connection)
     """
-
-    if HAVE_MYSQL_CONNECTOR is False:
-        base3.oao('Python module "mysql.connector" is not installed.', STATE_UNKNOWN)
-    if HAVE_MYSQL_CONNECTOR is False:
-        base3.oao('Python module "MySQLdb.cursors" is not installed.', STATE_UNKNOWN)
-
     try:
-        conn = mysql.connector.connect(**mysql_connection)
-    except Exception as e:
-        return(False, 'Connecting to DB failed, Error: {}'.format(e))
+        if mysql.connector.__version__ >= '2.1.1':
+            conn = mysql.connector.connect(**mysql_connection, use_pure = False)
+        else:
+            conn = mysql.connector.connect(**mysql_connection)
+    except mysql.connector.Error as e:
+        return (False, 'Connecting to DB failed, Error: {}'.format(e))
+    except ImportError as e:
+        # ImportError: MySQL Connector/Python C Extension not available
+        try:
+            conn = mysql.connector.connect(**mysql_connection, use_pure = True)
+        except mysql.connector.Error as e:
+            return (False, 'Connecting to DB failed, Error: {}'.format(e))
+
     return (True, conn)
 
 
@@ -90,12 +110,6 @@ def select(conn, sql, data={}, fetchone=False):
     of columns. A SELECT statement does not make any changes to the
     database.
     """
-
-    if HAVE_MYSQL_CONNECTOR is False:
-        base3.oao('Python module "mysql.connector" is not installed.', STATE_UNKNOWN)
-    if HAVE_MYSQL_CONNECTOR is False:
-        base3.oao('Python module "MySQLdb.cursors" is not installed.', STATE_UNKNOWN)
-
     if base3.version(mysql.connector.__version__) >= base3.version('2.0.0'):
         cursor = conn.cursor(dictionary=True)
     else:
@@ -110,4 +124,16 @@ def select(conn, sql, data={}, fetchone=False):
             return (True, [cursor.fetchone()])
         return (True, cursor.fetchall())
     except Exception as e:
-        return(False, 'Query failed: {}, Error: {}, Data: {}'.format(sql, e, data))
+        return (False, 'Query failed: {}, Error: {}, Data: {}'.format(sql, e, data))
+
+
+def vars2dict(vars):
+    """Converts
+    [{'Variable_name': 'a', 'Value': 'b'}, {'Variable_name': 'c', 'Value': 'd'}]
+    from `show variables;` to
+    {'a': 'b', 'c': 'd'}
+    """
+    myvar = {}
+    for row in vars:
+        myvar[row['Variable_name']] = row['Value']
+    return myvar
